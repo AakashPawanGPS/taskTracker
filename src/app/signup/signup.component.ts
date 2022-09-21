5aa8d3b8b15fb2317c93ab3e7145a60522147d9c
AakashPawanGPS <aakashpawangps@gmail.com>
2022-09-21 22:11:01 +0530

    Added Regex For Email SignUp

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -24,7 +24,7 @@ export class SignupComponent implements OnInit {
   signupForm: FormGroup = new FormGroup({
     fname: new FormControl('', [Validators.required]),
     lname: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
     password: new FormControl('', [Validators.required,Validators.minLength(8)])
   })
 
